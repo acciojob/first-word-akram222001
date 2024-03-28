29f552241026bbc5468c1ccd0099e79258851049
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-03-29 01:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 function firstWord(s) {
   // your code here
-	if(s === ''){
-		return '';
-	}
-	const spaceIndex = str.indexOf(' ');
-	if(spaceIndex === -1){
-		return s;
-	}
-	return s.substring(0, spaceIndex)
+  if (s === '') {
+    return '';
+  }
+  const spaceIndex = s.indexOf(' '); // Changed from str to s
+  if (spaceIndex === -1) {
+    return s;
+  }
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
- const s = prompt("Enter String:");
+const s = prompt("Enter String:");
 alert(firstWord(s));
